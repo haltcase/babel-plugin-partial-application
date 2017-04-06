93707f5b0c7e767ba2bc214efdd1c3098d25860a
citycide <thecitycide@gmail.com>
2017-04-06 01:36:29 -0500

    test: improve diff code, write expected if not exists

--- tests/index.js
+++ tests/index.js
@@ -1,10 +1,11 @@
 import test from 'ava'
 
 import { join } from 'path'
-import { readFileSync, readdirSync } from 'fs'
 import { transformFileSync } from 'babel-core'
+import { readFileSync, writeFileSync, readdirSync } from 'fs'
 
 const trim = str => str.replace(/^\s+|\s+$/, '')
+const normalizeLinebreaks = str => str.replace(new RegExp('\r\n', 'g'), '\n')
 
 const fixturesDir = join(__dirname, 'fixtures')
 const testFiles = readdirSync(fixturesDir)
@@ -19,8 +20,22 @@ testFiles.forEach(filePath => {
       plugins: [require('..')]
     })
 
-    const expected = readFileSync(expectedPath).toString()
+    let expected = ''
 
-    t.is(trim(code), trim(expected))
+    try {
+      expected = readFileSync(expectedPath).toString()
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        expected = code
+        writeFileSync(expectedPath, expected)
+      } else {
+        throw err
+      }
+    }
+
+    t.is(
+      normalizeLinebreaks(trim(code)),
+      normalizeLinebreaks(trim(expected))
+    )
   })
 })
