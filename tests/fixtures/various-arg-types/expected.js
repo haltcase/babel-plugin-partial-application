9ead36bdf6fd83a6ac2c15a40109cff9c9f8c79b
citycide <thecitycide@gmail.com>
2017-04-09 14:23:02 -0500

    feat: hoist complex function arguments from partials
    Prevent things like function calls and new expressions from being run each time a partial is called by hoisting them out  of the generated partial function.

--- tests/fixtures/various-arg-types/expected.js
+++ tests/fixtures/various-arg-types/expected.js
@@ -2,8 +2,12 @@ const something = [];
 const foo = () => 42;
 const H = class {};
 
+const _ref = foo();
+
+const _ref2 = new H();
+
 const partial = (_a, _a2) => {
-  return fn(_a, 1, _a2, true, null, 'some string', undefined, something, foo(), new H(), () => {});
+  return fn(_a, 1, _a2, true, null, 'some string', undefined, something, _ref, _ref2, () => {});
 };
 
 partial('hello', 'world');
