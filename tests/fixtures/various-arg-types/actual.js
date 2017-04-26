f445b0129898046f493babed5b32bbc8e38317c3
Bo Lingen <thecitycide@gmail.com>
2017-04-26 20:10:58 +0000

    test: add more arg types to test

--- tests/fixtures/various-arg-types/actual.js
+++ tests/fixtures/various-arg-types/actual.js
@@ -6,14 +6,18 @@ const partial = fn(
   _,
   1,
   _,
+  _.property,
   true,
   null,
+  _.method(),
   'some string',
   undefined,
   something,
+  fn(_.foo()),
   foo(),
   new H(),
-  () => {}
+  () => {},
+  ..._
 )
 
 partial('hello', 'world')
--- tests/fixtures/various-arg-types/expected.js
+++ tests/fixtures/various-arg-types/expected.js
@@ -2,12 +2,20 @@ const something = [];
 const foo = () => 42;
 const H = class {};
 
-const _ref = foo();
+const _ref = _obj2 => {
+  return _obj2.method();
+};
 
-const _ref2 = new H();
+const _ref2 = fn(_obj3 => {
+  return _obj3.foo();
+});
 
-const partial = (_arg, _arg2) => {
-  return fn(_arg, 1, _arg2, true, null, 'some string', undefined, something, _ref, _ref2, () => {});
+const _ref3 = foo();
+
+const _ref4 = new H();
+
+const partial = (_arg, _arg2, _obj, ..._spr) => {
+  return fn(_arg, 1, _arg2, _obj.property, true, null, _ref, 'some string', undefined, something, _ref2, _ref3, _ref4, () => {}, ..._spr);
 };
 
 partial('hello', 'world');
