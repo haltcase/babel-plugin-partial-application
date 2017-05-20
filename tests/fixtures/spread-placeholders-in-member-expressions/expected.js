b5d4465a785a6ed5c3b420815c08d4f3c898ae48
Bo Lingen <thecitycide@gmail.com>
2017-05-20 04:56:28 +0000

    fix: spread placeholder compilation

--- tests/fixtures/spread-placeholders-in-member-expressions/expected.js
+++ tests/fixtures/spread-placeholders-in-member-expressions/expected.js
@@ -1,11 +1,11 @@
 (_obj, ..._spr) => {
-  return _obj.foo(..._);
+  return _obj.foo(..._spr);
 };
 
 (_obj2, _arg, ..._spr2) => {
   return _obj2.foo(_arg, ..._spr2);
 };
 
-(_obj3, _obj4) => {
-  return _obj3.foo(_obj4.bar, ..._);
+(_obj3, _obj4, ..._spr3) => {
+  return _obj3.foo(_obj4.bar, ..._spr3);
 };
