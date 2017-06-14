df44173be86bfb7fa961c05c666deadf09d9c74d
Bo Lingen <thecitycide@gmail.com>
2017-06-14 20:27:54 +0000

    fix: nested positional 'container is falsy' error

--- tests/fixtures/positional-placeholders-nested/actual.js
+++ tests/fixtures/positional-placeholders-nested/actual.js
@@ -1,3 +1,5 @@
 const apply = _`2`(_`1`)
 
 const applyWith = _`2`(_`1`, _, 'static')
+
+const withProperty = _`2`(_`1`.foo)
--- tests/fixtures/positional-placeholders-nested/expected.js
+++ tests/fixtures/positional-placeholders-nested/expected.js
@@ -5,3 +5,7 @@ const apply = (_pos2, _pos) => {
 const applyWith = (_pos4, _pos3, _arg) => {
   return _pos3(_pos4, _arg, 'static');
 };
+
+const withProperty = (_pos6, _pos5) => {
+  return _pos5(_pos6.foo);
+};
